167d7ea63485e37889a003ba713a951ceec62569
eurusov <eurusov@users.noreply.github.com>
2019-10-12 03:24:30 +0300

    Setting up CharacterEncodingFilter.

--- src/main/java/spring_test/config/AppInitializer.java
+++ src/main/java/spring_test/config/AppInitializer.java
@@ -1,6 +1,9 @@
 package spring_test.config;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
 
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
@@ -18,5 +21,15 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return new Filter[]{
+                characterEncodingFilter,
+        };
     }
 }
