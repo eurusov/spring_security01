d3325e1a7a46850f55fafad774ed9b83f766a17c
eurusov <eurusov@users.noreply.github.com>
2019-10-19 13:00:55 +0300

    New user methods moved from UserController to AnonymousController.

--- src/main/java/spring_test/controller/AnonymousController.java
+++ src/main/java/spring_test/controller/AnonymousController.java
@@ -1,10 +1,18 @@
 package spring_test.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import spring_test.model.User;
 import spring_test.service.UserService;
 
 @Controller
+@RequestMapping("/")
 public class AnonymousController {
     private UserService userService;
 
@@ -13,5 +21,16 @@ public class AnonymousController {
         this.userService = userService;
     }
 
+    @GetMapping("/new")
+    public String showNewUserForm(Model model) {
+        model.addAttribute("user", new User());
+        return "new";
+    }
 
+    @PostMapping("/saveNewUser")
+    public String saveNewUser(@ModelAttribute("user") User user) {
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        userService.addUser(user);
+        return "redirect:/";
+    }
 }
--- src/main/java/spring_test/controller/UserController.java
+++ src/main/java/spring_test/controller/UserController.java
@@ -1,10 +1,12 @@
 package spring_test.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import spring_test.model.User;
 import spring_test.service.UserService;
@@ -27,19 +29,6 @@ public class UserController {
         User loggedUser = userService.getUserByUsername(principal.getName());
         model.addAttribute("principal", loggedUser);
         return "user/user";
-    }
-
-    @GetMapping("/new")
-    public String showNewUserForm(Model model) {
-        model.addAttribute("user", new User());
-        return "new";
-    }
-
-    @PostMapping("/saveNewUser")
-    public String saveNewUser(@ModelAttribute("user") User user) {
-        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
-        userService.addUser(user);
-        return "redirect:/";
     }
 
     @GetMapping("/edit")
